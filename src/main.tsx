03211e6919f6fc96100594ba62d97c538f4ca1f4
Victor Oliva <olivarra1@gmail.com>
2025-08-30 13:49:55 +0800

    handling multiple chains

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,26 @@
+import { pas, dot } from "@polkadot-api/descriptors";
+import { CompatibilityLevel, createClient } from "polkadot-api";
+import { getWsProvider } from "polkadot-api/ws-provider/web";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
-import { Binary, CompatibilityLevel, createClient } from "polkadot-api";
-import { getSmProvider } from "polkadot-api/sm-provider";
-import { start } from "polkadot-api/smoldot";
-import { chainSpec } from "polkadot-api/chains/paseo";
-import { chainSpec as peopleChainSpec } from "polkadot-api/chains/paseo_people";
-import { pas } from "@polkadot-api/descriptors";
+import "./index.css";
 
-const smoldot = start();
-
-const relayChain = smoldot.addChain({
-  chainSpec,
-  disableJsonRpc: true,
-});
-// const paraChain = relayChain.then((rc) =>
-//   smoldot.addChain({
-//     chainSpec: peopleChainSpec,
-//     potentialRelayChains: [rc],
-//   })
-// );
-
-const client = createClient(getSmProvider(relayChain));
-
-// client.finalizedBlock$.subscribe((block) => {
-//   console.log("block", block);
-// });
-
-// client.blocks$.subscribe(block => {
-//   typedApi.query.System.Events.getValue({
-//     at: block.hash
-//   })
-// })
-
+const client = createClient(getWsProvider("wss://paseo.rpc.amforc.com"));
 const typedApi = client.getTypedApi(pas);
+const dotTypedApi = client.getTypedApi(dot);
 
-// typedApi.tx.Referenda.submit({
-//   enactment_moment: {
-//     type: "After",
-//     value: 0,
-//   },
-//   proposal: {
-//     type: "Inline",
-//     value: Binary.fromHex("0x"),
-//   },
-//   proposal_origin: {
-//     type: "Origins",
-//     value: {} as any,
-//   },
-// });
+const res = await typedApi.query.System.Account.getValue(
+  "5FxrUu1PUugUYs6HQ83bDswjGLyHYTEzm7yqmrkKVPaYe71Y"
+);
+console.log("my account", res);
 
-typedApi.query.System.Account.getValue("asdf");
+if (await typedApi.query.Sudo.Key.isCompatible(CompatibilityLevel.Identical)) {
+  const key = await typedApi.query.Sudo.Key.getValue();
+  console.log("sudo key", key);
+} else {
+  await dotTypedApi.query;
+}
 
 typedApi.query.System.Events.watchValue().subscribe((events) => {
   console.log(events);
