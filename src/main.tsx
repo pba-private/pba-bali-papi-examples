b3362bf818fdacdb9dc7a6ec80f97a2a5cfa033b
Victor Oliva <olivarra1@gmail.com>
2025-08-30 14:07:53 +0800

    exercies description

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { pas, dot } from "@polkadot-api/descriptors";
-import { CompatibilityLevel, createClient } from "polkadot-api";
+import { pas } from "@polkadot-api/descriptors";
+import { createClient, type SS58String } from "polkadot-api";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -8,29 +8,19 @@ import "./index.css";
 
 const client = createClient(getWsProvider("wss://paseo.rpc.amforc.com"));
 const typedApi = client.getTypedApi(pas);
-const dotTypedApi = client.getTypedApi(dot);
-
-const token = await typedApi.compatibilityToken;
-
-function calculateSomethingAboutBounties() {
-  const result = typedApi.constants.Bounties.BountyValueMinimum(token);
-}
 
 const res = await typedApi.query.System.Account.getValue(
   "5FxrUu1PUugUYs6HQ83bDswjGLyHYTEzm7yqmrkKVPaYe71Y"
 );
 console.log("my account", res);
 
-if (typedApi.query.Sudo.Key.isCompatible(CompatibilityLevel.Identical, token)) {
-  const key = await typedApi.query.Sudo.Key.getValue();
-  console.log("sudo key", key);
-} else {
-  await dotTypedApi.query;
-}
+async function getSudoAccountFreeBalance(): Promise<bigint> {}
 
-typedApi.query.System.Events.watchValue().subscribe((events) => {
-  console.log(events);
-});
+async function findAllProxyAccountsOfTypeAny(): Promise<Array<SS58String>> {}
+
+async function findMaxProxyAccounts(): Promise<Array<SS58String>> {
+  // Find the proxy accounts that have the maximum allowed proxy delegates
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
