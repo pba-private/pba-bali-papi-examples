6847b947f114c0ec2603e6b191a50f2ecbe22486
Victor Oliva <olivarra1@gmail.com>
2025-08-31 15:59:44 +0800

    connect extension, submit transaction

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,57 @@
 import { pas } from "@polkadot-api/descriptors";
-import { createClient, type SS58String } from "polkadot-api";
+import {
+  Binary,
+  createClient,
+  InvalidTxError,
+  type SS58String,
+} from "polkadot-api";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { withPolkadotSdkCompat } from "polkadot-api/polkadot-sdk-compat";
+import {
+  getInjectedExtensions,
+  connectInjectedExtension,
+} from "polkadot-api/pjs-signer";
 
 const client = createClient(
   withPolkadotSdkCompat(getWsProvider("wss://paseo.rpc.amforc.com"))
 );
 const typedApi = client.getTypedApi(pas);
+
+const res = getInjectedExtensions();
+
+const extension = await connectInjectedExtension(res[0]);
+
+const account = extension.getAccounts()[0];
+
+console.log(account);
+
+setTimeout(async () => {
+  const tx = typedApi.tx.System.remark({
+    remark: Binary.fromText("Hello PBA!"),
+  });
+
+  // const r = await tx.signAndSubmit(account.polkadotSigner);
+  tx.signSubmitAndWatch(account!.polkadotSigner).subscribe((evt) => {
+    switch (evt.type) {
+      case "signed":
+        console.log("signed", evt.txHash);
+        break;
+      case "broadcasted":
+        console.log("broadcasted", evt.txHash);
+        break;
+      case "txBestBlocksState":
+        console.log("in a best block", evt);
+        break;
+      case "finalized":
+        console.log("finalized", evt);
+        break;
+    }
+  });
+}, 1000);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
