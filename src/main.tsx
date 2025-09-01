9b32abebe6c7298b4268434160a6429f642b0281
Victor Oliva <olivarra1@gmail.com>
2025-09-01 11:12:05 +0800

    correlate solution

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import { pas } from "@polkadot-api/descriptors";
-import { createClient } from "polkadot-api";
 import { chainSpec } from "polkadot-api/chains/polkadot";
 import { getSmProvider } from "polkadot-api/sm-provider";
-import type { JsonRpcProvider } from "polkadot-api/ws-provider/web";
 import { start } from "polkadot-api/smoldot";
+import type { JsonRpcProvider } from "polkadot-api/ws-provider/web";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -18,13 +16,13 @@ const smoldotProvider = getSmProvider(chain);
 
 const provider: JsonRpcProvider = (onMsg) => {
   const smoldotConnection = smoldotProvider((msg) => {
-    console.log(JSON.parse(msg));
+    console.log(msg);
     onMsg(msg);
   });
 
   return {
     send(message) {
-      console.log(JSON.parse(message));
+      console.log(message);
       smoldotConnection.send(message);
     },
     disconnect() {
@@ -34,11 +32,39 @@ const provider: JsonRpcProvider = (onMsg) => {
 };
 
 let id = 0;
+let followSubscription: string | null = null;
 
-const connection = provider((msg) => {
+const requestIdToResolveFn = new Map<number, (value: string[]) => void>();
+const operationIdToResolveFn = new Map<string, (value: string[]) => void>();
+
+const connection = provider(async (msg) => {
   const parsedMsg = JSON.parse(msg);
 
   // Figure out follow subscription
+  if (parsedMsg.id === followId) {
+    followSubscription = parsedMsg.result;
+  }
+
+  const resolveFn = requestIdToResolveFn.get(parsedMsg.id);
+  if (resolveFn) {
+    requestIdToResolveFn.delete(parsedMsg.id);
+    operationIdToResolveFn.set(parsedMsg.result.operationId, resolveFn);
+  }
+
+  if (
+    parsedMsg.method === "chainHead_v1_followEvent" &&
+    parsedMsg.params.result.event === "operationBodyDone"
+  ) {
+    const operationId = parsedMsg.params.result.operationId;
+    const value = parsedMsg.params.result.value;
+
+    const resolveFn = operationIdToResolveFn.get(operationId);
+    if (!resolveFn) {
+      console.warn("unknown operation id", operationId);
+      return;
+    }
+    resolveFn(value);
+  }
 
   if (
     parsedMsg.method === "chainHead_v1_followEvent" &&
@@ -46,19 +72,51 @@ const connection = provider((msg) => {
   ) {
     sendFollow();
   }
+
+  if (
+    parsedMsg.method === "chainHead_v1_followEvent" &&
+    parsedMsg.params.result.event === "finalized"
+  ) {
+    const results = await Promise.all(
+      parsedMsg.params.result.finalizedBlockHashes.map((hash: string) =>
+        getBody(hash)
+      )
+    );
+    console.log("all bodies", results);
+  }
 });
 
-function getBody(blockHash: string) {
-  // TODO
-  // You need follow subscription
-  // connection.send(...)
+function getBody(blockHash: string): Promise<string[]> {
+  if (followSubscription == null) {
+    throw new Error("Follow not ready yet");
+  }
+
+  return new Promise<string[]>((resolve) => {
+    const reqId = ++id;
+
+    requestIdToResolveFn.set(reqId, resolve);
+
+    connection.send(
+      JSON.stringify({
+        jsonrpc: "2.0",
+        id: reqId,
+        method: "chainHead_v1_body",
+        params: [followSubscription, blockHash],
+      })
+    );
+  });
 }
 
+(window as any).getBody = getBody;
+
+let followId: number | null = null;
 function sendFollow() {
+  followId = ++id;
+
   connection.send(
     JSON.stringify({
       jsonrpc: "2.0",
-      id: ++id,
+      id: followId,
       method: "chainHead_v1_follow",
       params: [true],
     })
