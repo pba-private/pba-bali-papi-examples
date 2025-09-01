3d39c443c1d1060b3d165acbc8ca99cde13c782e
Victor Oliva <olivarra1@gmail.com>
2025-09-01 08:31:20 +0800

    provider + follow

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { pas } from "@polkadot-api/descriptors";
 import { createClient } from "polkadot-api";
 import { chainSpec } from "polkadot-api/chains/polkadot";
 import { getSmProvider } from "polkadot-api/sm-provider";
+import type { JsonRpcProvider } from "polkadot-api/ws-provider/web";
 import { start } from "polkadot-api/smoldot";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -14,10 +15,37 @@ const chain = smoldot.addChain({
   chainSpec,
 });
 
-const client = createClient(
-  withLogsRecorder(console.log, getSmProvider(chain))
+const smoldotProvider = getSmProvider(chain);
+
+const provider: JsonRpcProvider = (onMsg) => {
+  const smoldotConnection = smoldotProvider((msg) => {
+    console.log(`<<-${Date.now()}-${msg}`);
+    onMsg(msg);
+  });
+
+  return {
+    send(message) {
+      console.log(`>>-${Date.now()}-${message}`);
+      smoldotConnection.send(message);
+    },
+    disconnect() {
+      smoldotConnection.disconnect();
+    },
+  };
+};
+
+const connection = provider((msg) => {
+  // TODO
+});
+
+connection.send(
+  JSON.stringify({
+    jsonrpc: "2.0",
+    id: "0",
+    method: "chainHead_v1_follow",
+    params: [true],
+  })
 );
-const typedApi = client.getTypedApi(pas);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
