5ef5b4ff42488aa991c330ce19491544157b4c2e
Victor Oliva <olivarra1@gmail.com>
2025-09-03 07:56:13 +0800

    chopsticks setStorage

--- src/main.tsx
+++ src/main.tsx
@@ -1,61 +1,33 @@
 import { chainSpec } from "polkadot-api/chains/polkadot";
 import { getSmProvider } from "polkadot-api/sm-provider";
 import { start } from "polkadot-api/smoldot";
-import type { JsonRpcProvider } from "polkadot-api/ws-provider/web";
-import { createClient } from "@polkadot-api/substrate-client";
+import {
+  getWsProvider,
+  type JsonRpcProvider,
+} from "polkadot-api/ws-provider/web";
 import { decAnyMetadata, u32 } from "@polkadot-api/substrate-bindings";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { toHex } from "polkadot-api/utils";
+import { createClient } from "polkadot-api";
 
-const smoldot = start();
-const chain = smoldot.addChain({
-  chainSpec,
-});
+const client = createClient(getWsProvider("ws://localhost:8000"));
 
-const smoldotProvider = getSmProvider(chain);
+console.log("Creating new block");
+const block = await client._request("dev_newBlock", []);
 
-const provider: JsonRpcProvider = (onMsg) => {
-  const smoldotConnection = smoldotProvider((msg) => {
-    console.log(msg);
-    onMsg(msg);
-  });
+console.log("New block", block);
 
-  return {
-    send(message) {
-      console.log(message);
-      smoldotConnection.send(message);
-    },
-    disconnect() {
-      smoldotConnection.disconnect();
-    },
-  };
-};
-
-const client = createClient(provider);
-
-const chainHead = client.chainHead(
-  true,
-  async (evt) => {
-    console.log(evt);
-
-    if (evt.type === "initialized") {
-      const result = await chainHead.call(
-        evt.finalizedBlockHashes[0],
-        "Metadata_metadata_at_version",
-        toHex(u32.enc(15))
-      );
-
-      const metadata = decAnyMetadata(result);
-      console.log("metadata", metadata);
-    }
-  },
-  (err) => {
-    console.log("error", err);
-  }
-);
+await client._request("dev_setStorage", [
+  [
+    [
+      "0x5c0d1176a568c1f92944340dbfed9e9c530ebca703c85910e7164cb7d1c9e47b",
+      "0xac7a7e96dd3562f7b16dbfc0db669d2facd2489ef5116ce25d5dbd4d4141214c",
+    ],
+  ],
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
