22bdc0ae66907f34801b16ab61b0ba13b241fbcb
Victor Oliva <olivarra1@gmail.com>
2025-09-05 11:32:40 +0800

    referenda exercise solution

--- src/rxjs-tests.ts
+++ src/rxjs-tests.ts
@@ -1,20 +1,60 @@
 import { createClient } from "polkadot-api";
-import { Observable, map, interval } from "rxjs";
+import { Observable, map, interval, switchMap, EMPTY, from } from "rxjs";
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 import { dot } from "@polkadot-api/descriptors";
 
-const client = createClient(
-  getWsProvider("wss://polkadot-rpc.publicnode.com")
-);
+const client = createClient(getWsProvider("wss://polkadot-rpc.publicnode.com"));
 const typedApi = client.getTypedApi(dot);
 
 const ACCOUNT = "1jbZxCFeNMRgVRfggkknf8sTWzrVKbzLvRuLWvSyg9bByRG";
 const TRACK = 33;
 
+const getVoteDirection = (vote: number) =>
+  vote & 0x80 ? ("aye" as const) : ("nay" as const);
 
-const getVoteDirection = (vote: number) => vote & 0x80 ? "aye" as const : "nay" as const;
+const referendaWithSameOutcome$ =
+  typedApi.query.ConvictionVoting.VotingFor.watchValue(ACCOUNT, TRACK).pipe(
+    switchMap((voteInfo) => {
+      if (voteInfo.type === "Delegating") return EMPTY;
 
-// query.ConvictionVoting.VotingFor.watchValue(account, track)
-// query.Referenda.ReferendumInfoFor.getValues([number][])
+      const votes = voteInfo.value.votes
+        .filter(([, vote]) => vote.type === "Standard")
+        .map(([refId, vote]) => ({
+          refId,
+          voteDirection:
+            vote.type === "Standard"
+              ? getVoteDirection(vote.value.vote)
+              : // unreachable
+                "aye",
+        }));
 
-const referendaWithSameOutcome$ = /* ... */
+      return from(
+        typedApi.query.Referenda.ReferendumInfoFor.getValues(
+          // votes.map(({ refId }) => [refId])
+          votes.map((vote) => [vote.refId])
+        )
+      ).pipe(
+        map((referenda) => {
+          const result: number[] = [];
+
+          referenda.forEach((referendum, i) => {
+            if (referendum?.type !== "Ongoing") return;
+
+            const voteDirection = votes[i].voteDirection;
+            const currentOutcome =
+              referendum.value.tally.ayes > referendum.value.tally.nays
+                ? ("aye" as const)
+                : ("nay" as const);
+
+            if (voteDirection === currentOutcome) {
+              result.push(votes[i].refId);
+            }
+          });
+
+          return result;
+        })
+      );
+    })
+  );
+
+referendaWithSameOutcome$.subscribe(console.log);
